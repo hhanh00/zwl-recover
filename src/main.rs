766d74f4d75b9d86d3323e2b29795a553775f6d1
Hanh <hanh425@gmail.com>
2024-12-20 22:14:47 +1000

    feat: Add CI

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use anyhow::Result;
 use clap::Parser;
 use zcash_warp::{
     coin::CoinDef,
-    data::fb::{PaymentRequestT, RecipientT},
+    data::fb::{ConfigT, PaymentRequestT, RecipientT},
     db::{
         account::get_account_info,
         account_manager::{create_new_account, create_transparent_address},
@@ -48,7 +48,12 @@ async fn main() -> Result<()> {
 
     let args = Args::parse();
     let Args {
-        seed, birth_height, ntaddrs, nzaddrs, destination, lwd_url
+        seed,
+        birth_height,
+        ntaddrs,
+        nzaddrs,
+        destination,
+        lwd_url,
     } = args;
 
     let home_dir = std::env::var("HOME").unwrap();
@@ -56,7 +61,9 @@ async fn main() -> Result<()> {
 
     let mut zec = CoinDef::from_network(0, Network::Main);
     zec.set_path_password("zwl-recover.db", "")?;
-    zec.set_single_lwd_url(lwd_url.to_string())?;
+    let mut config = ConfigT::default();
+    config.servers = Some(vec![lwd_url.to_string()]);
+    zec.set_config(&config)?;
 
     let mut connection = zec.connection()?;
     // 1. create db
@@ -107,31 +114,40 @@ async fn main() -> Result<()> {
     warp_synchronize(&zec, end_height).await?;
 
     // 5. check balance
-    let zbal = connection.query_row(
-        "SELECT SUM(value) FROM notes WHERE spent IS NULL",
-        [],
-        |r| r.get::<_, Option<u64>>(0),
-    )?.unwrap_or_default();
-    let tbal = connection.query_row(
-        "SELECT SUM(value) FROM utxos WHERE spent IS NULL",
-        [],
-        |r| r.get::<_, Option<u64>>(0),
-    )?.unwrap_or_default();
+    let zbal = connection
+        .query_row(
+            "SELECT SUM(value) FROM notes WHERE spent IS NULL",
+            [],
+            |r| r.get::<_, Option<u64>>(0),
+        )?
+        .unwrap_or_default();
+    let tbal = connection
+        .query_row(
+            "SELECT SUM(value) FROM utxos WHERE spent IS NULL",
+            [],
+            |r| r.get::<_, Option<u64>>(0),
+        )?
+        .unwrap_or_default();
     println!("{zbal} {tbal}");
 
     // 6. make sweep txs
-    let sweep = |table: &'static str| {
+    let sweep = |table: &'static str, id_account: Option<u32>| {
         let zec = zec.clone();
         let destination = destination.clone();
         async move {
             let mut client = zec.connect_lwd()?;
             let connection = zec.connection()?;
-            let mut s = connection.prepare(&format!(
-                "SELECT account, SUM(value) AS bal FROM {table} WHERE spent IS NULL
+            let zaccounts = match id_account {
+                Some(id_account) => vec![id_account],
+                None => {
+                    let mut s = connection.prepare(&format!(
+                        "SELECT account, SUM(value) AS bal FROM {table} WHERE spent IS NULL
                 GROUP BY account"
-            ))?;
-            let rows = s.query_map([], |r| r.get::<_, u32>(0))?;
-            let zaccounts = rows.collect::<Result<Vec<_>, _>>()?;
+                    ))?;
+                    let rows = s.query_map([], |r| r.get::<_, u32>(0))?;
+                    rows.collect::<Result<Vec<_>, _>>()?
+                }
+            };
 
             for zaccount in zaccounts {
                 // 6a. create unsigned tx - show fees
@@ -175,8 +191,8 @@ async fn main() -> Result<()> {
         }
     };
 
-    sweep("utxos").await?;
-    sweep("notes").await?;
+    sweep("utxos", Some(tid)).await?;
+    sweep("notes", None).await?;
 
     Ok(())
 }
